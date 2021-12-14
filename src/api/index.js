748b384a709adb56c5a62ff8ac2fb137e360ab79
liron <lirongolshmid11@gmail.com>
2021-12-14 17:03:33 +0200

    .

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://192.168.1.200:5000' });
+const API = axios.create({ baseURL: 'https://travelogapi.herokuapp.com' });
 
 API.interceptors.request.use(req => {
    if (localStorage.getItem('profile')) {
